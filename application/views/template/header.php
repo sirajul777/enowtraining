3f5d9aeb8b27fd107a6bbeae8c67f17b28871977
sirajul777 <thony.raj880@gmail.com>
2020-02-24 23:45:12 +0800

    new add css color

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -19,7 +19,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <!--Import materialize.css-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="<?= base_url('assets'); ?>/css/materialize.css" media="screen,projection" />
-    <link type="text/css" rel="stylesheet" href="<?= base_url('assets'); ?>/css/enow.css" media="screen,projection" />
+    <link type="text/css" rel="stylesheet" href="<?= base_url('assets'); ?>/css/enow.css" />
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
